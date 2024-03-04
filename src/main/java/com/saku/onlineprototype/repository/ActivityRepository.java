1f3738356ddc568f1b55ff5af47062ecdf6d8b10
Teerut26 <teerutsr@gmail.com>
2024-03-04 19:31:02 +0700

    Refactor Dockerfile and update entity and repository names

--- src/main/java/com/saku/onlineprototype/repository/ActivityRepository.java
+++ src/main/java/com/saku/onlineprototype/repository/ActivityRepository.java
@@ -3,9 +3,11 @@ package com.saku.onlineprototype.repository;
 
 import com.saku.onlineprototype.entity.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ActivityRepository extends JpaRepository<Activity,String> {
     Optional<Activity> searchByActivityID(String activityID);
 }
--- src/main/java/com/saku/onlineprototype/repository/OrganizationRepository.java
+++ src/main/java/com/saku/onlineprototype/repository/OrganizationRepository.java
@@ -2,6 +2,8 @@ package com.saku.onlineprototype.repository;
 
 import com.saku.onlineprototype.entity.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrganizationRepository extends JpaRepository<Organization,String> {
 }
